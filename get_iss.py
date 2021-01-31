86b31028bdc661de0ca8d876e206d9441b649f98
aurelien fache <aurelien.fache@gmail.com>
2021-01-31 13:26:54 +0100

    first version

---
+++ get_iss.py
@@ -0,0 +1,55 @@
+from skyfield.api import load, wgs84
+
+from datetime import date
+from datetime import datetime
+
+today = date.today()
+
+stations_url = 'http://celestrak.com/NORAD/elements/stations.txt'
+satellites = load.tle_file(stations_url)
+print('Loaded', len(satellites), 'satellites')
+
+by_name = {sat.name: sat for sat in satellites}
+satellite = by_name['ISS (ZARYA)']
+#print(satellite)
+print ("ISS (ZARYA) catalog #25544 \nepoch 2021-01-15 06:17:01 UTC")
+#print(satellite.epoch.utc_jpl())
+
+ts = load.timescale()
+t = ts.now()
+
+# datetime object containing current date and time
+now = datetime.now()
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+print()
+print("date and time =", dt_string)	
+
+geocentric = satellite.at(t)
+#print(geocentric.position.km)
+
+subpoint = wgs84.subpoint(geocentric)
+print ()
+print('Latitude:', subpoint.latitude)
+print('Longitude:', subpoint.longitude)
+print('Elevation (m):', int(subpoint.elevation.m))
+print ()
+
+me = wgs84.latlon(48.873459,2.358040)
+
+difference = satellite - me
+#print(difference)
+
+topocentric = difference.at(t)
+#print(topocentric.position.km)
+
+alt, az, distance = topocentric.altaz()
+
+if alt.degrees > 0:
+    print( 'The ISS is above the horizon')
+
+#print (alt.degrees)
+print("Altitude: ", alt)
+print ("Azimut: ",az)
+print ()
+s = "The distance between my geographical \nposition and the ISS:"
+print(s,int(distance.km), 'km')
---
+++ lego_light.py
@@ -0,0 +1,51 @@
+import logging
+from curio import sleep
+from bricknil import attach, start
+from bricknil.hub import PoweredUpHub
+from bricknil.sensor import Light
+
+
+@attach(Light, name='light1')
+@attach(Light, name='light2')
+
+#@attach(ExternalMotor, name='motor')
+# Report back when motor speed changes. You must have a motor_change method defined
+#@attach(ExternalMotor, name='motor', capabilities=['sense_speed'])
+class Eiffel(PoweredUpHub):
+
+    async def motor_change(self):
+         self.message_info("Running")
+
+
+    async def run(self):
+        self.message_info("Running")
+        self.keep_running = True
+        brightness = 0
+        delta = 10
+
+        while self.keep_running:
+            # change the brightness up and down between -100 and 100
+            brightness += delta
+            if brightness >= 100:
+                delta = -10
+            elif brightness <= -100:
+                delta = 10
+            #self.message_info("Brightness: {}".format(brightness))
+            await self.light1.set_brightness(100)
+            await self.light2.set_brightness(100)
+            await sleep(0.3)
+            await self.light1.set_brightness(0)
+            await self.light2.set_brightness(0)
+
+            #await self.motor.set_speed(100)   # Setting the speed
+            #await self.motor.rotate(90, speed=-50) # Turn 60 degrees counter-clockwise from current position
+            #await self.motor.set_pos(90, speed=20) # Turn clockwise to 3 o'clock position
+            await sleep(0.3)
+
+
+async def system():
+    Eiffel('eiffel_light')
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    start(system)
